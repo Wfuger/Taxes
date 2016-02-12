8d550a52e4d8b1a7dc2acc63238d7a2af8cc730c
Wfuger <wfuger@gmail.com>
2016-02-12 12:52:14 -0700

    tests passed

---
+++ main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  taxCalculator: function(dollars) {
+    var tax = 0;
+    if (dollars <= 10) {
+      tax = dollars * 0.1
+      return tax;
+    }
+    if (dollars <= 20) {
+      tax = ((dollars - 10) * 0.07) + 1;
+      return tax;
+    }
+    if (dollars <= 30) {
+      tax = ((dollars - 20) * 0.05) + 1.7;
+      return tax;
+    } else {
+      tax = ((dollars - 10) * 0.03) + 2.2;
+      return tax;
+    }
+  }
+}
